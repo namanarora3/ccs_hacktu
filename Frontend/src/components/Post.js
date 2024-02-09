597f3eb3a5b2057ce94ba78aa154a81910229ec4
Daksh Raheja <dakshraheja03@gmail.com>
2024-02-10 03:27:00 +0530

    fixed post page

--- Frontend/src/components/Post.js
+++ Frontend/src/components/Post.js
@@ -1,5 +1,5 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { useRouter } from 'next/router';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
@@ -24,14 +24,58 @@ const StyledBox = styled(Box)(({ theme }) => ({
   }
 }));
 
-const CommentSection = ({ issue, onUpvote, onCommentSubmit }) => {
+const CommentSection =() => {
   const [comments, setComments] = useState('');
+  
+    const router = useRouter();
+    const [image,setImage]=useState('');
+    const [issue, setIssue] = useState([]);
+    const { id } =router.query;
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    onCommentSubmit(comments);
+    const token=localStorage.getItem('token')
+    const response = await fetch(`http://127.0.0.1:8001/issue/comments/${id}/`, {
+      method: 'POST',
+      headers: {
+        'Authorization':`Token ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"content":comments})
+    });
+
+    if (response.ok) {
+      console.log('Text submitted successfully');
+    } else {
+      console.error('Failed to submit text');
+    }
+    console.log(comments)
     setComments('');
   };
+
+  useEffect(() => {
+    const token=localStorage.getItem('token')
+    const fetchData = async () => {
+      if (id) {
+        const response = await fetch(`http://127.0.0.1:8001/issue/${id}/`,{
+          headers:{
+            'Authorization':`Token ${token}`
+          }
+        });
+        if (response.ok) {
+          const data = await response.json();
+          if(data){
+            setIssue(data?.data);
+            setImage(data?.data.image)
+            console.log(image)
+          }
+        } else {
+          setError('Failed to fetch post data');
+        }
+      }
+    };
+    fetchData();
+  }, [id]); 
 
   return (
     <Card sx={{ marginBottom: 2 }}>
@@ -42,38 +86,37 @@ const CommentSection = ({ issue, onUpvote, onCommentSubmit }) => {
               <Box sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
                 <Avatar alt='Name' src='/images/avatars/4.png' sx={{ width: 34, height: 34, marginRight: 2.75 }} />
                 <Typography variant='body5' sx={{ color: 'common.black' }}>
-                  {issue.username}
+                  {issue.user_name}
                 </Typography>
               </Box>
             </Box>
             <br />
             <Typography sx={{ marginBottom: 3.5, fontSize: 20, fontWeight: 700 }}>
-              {issue.issueName}
+              {issue.title}
             </Typography>
             <Typography variant='body2'>{issue.issueDescription}</Typography>
-            <Box>
-              <Image src={issue.image} width={'400%'} height={'300%'} alignItems = {'center'} alt='card' />
-            </Box>
+            <Box sx={{marginTop:5}}>
+              <Image src={"http://127.0.0.1:8001/media/images/issues/cat_v97lMyW.webp"} width={'400%'} height={'300%'} alignItems = {'center'} alt='card' /></Box>
             <Divider sx={{ marginTop: 6.5, marginBottom: 6.75 }} />
             <Grid container spacing={4}>
               <Grid item xs={12} sm={5}>
                 <StyledBox>
                   <Box sx={{ mb: 6.75, display: 'flex', alignItems: 'center' }}>
                     <LockOpenOutline sx={{ color: 'primary.main', marginRight: 2.75 }} fontSize='small' />
-                    <Typography variant='body2'>LOCATION: {issue.location}</Typography>
+                    <Typography variant='body2'>LOCATION: {issue.long}</Typography>
                   </Box>
                   <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <AccountOutline sx={{ color: 'primary.main', marginRight: 2.75 }} fontSize='small' />
-                    <Typography variant='body2'>DATE: {issue.date}</Typography>
+                    <Typography variant='body2'>DATE: {issue.created}</Typography>
                   </Box>
                 </StyledBox>
               </Grid>
               <Grid item xs={12} sm={7}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                  <Button onClick={onUpvote} color="primary" sx={{ marginRight: 1 }}>
-                    <StarOutline sx={{ color: 'primary.main', variant: 'outlined', alignSelf: 'left' }} fontSize='small' />
-                    <Typography variant='body2'>Upvote {issue.upvoteCount}</Typography>
-                  </Button>
+                  {/* <Button onClick={onUpvote} color="primary" sx={{ marginRight: 1 }}>
+                    <StarOutline sx={{ color: 'primary.main', variant: 'outlined', alignSelf: 'left', marginLeft:-3 }} fontSize='small' />
+                    <Typography variant='body2' sx={{marginLeft:3}}>Upvote {issue.like_count}</Typography>
+                  </Button> */}
                   <Box sx={{ mb: 1, display: 'flex', alignItems: 'center' }}>
                     <TrendingUp sx={{ color: 'primary.main', marginRight: 2.75 }} fontSize='small' />
                     <Typography variant='body2'>Category: {issue.category}</Typography>
@@ -127,7 +170,7 @@ const CommentSection = ({ issue, onUpvote, onCommentSubmit }) => {
           xs={12}
           sx={{ paddingTop: ['0 !important', '1.5rem !important'], paddingLeft: ['1.5rem !important', '0 !important'] }}
         >
-          {/* <CardContent
+          <CardContent
             sx={{
               height: '100%',
               display: 'flex',
@@ -138,7 +181,7 @@ const CommentSection = ({ issue, onUpvote, onCommentSubmit }) => {
               padding: (theme) => `${theme.spacing(18, 5, 16)} !important`
             }}
           >
-          </CardContent> */}
+          </CardContent>
         </Grid>
       </Grid>
     </Card>
